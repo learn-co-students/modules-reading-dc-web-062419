7cb914a705de8ab473e302d6111a377e40eda5d0
Trevahr Robertson <Trevahr@gmail.com>
2019-06-27 23:58:03 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,23 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+require_relative './kid.rb'
+
+class Dancer
+     
+     extend FancyDance::ClassMethods
+     include FancyDance::InstanceMethods
+    
+     attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+    
+end
+
+angelina = Dancer.new("angelina")
+puts angelina.twirl
+buster = Kid.new("buster")
+puts buster.jump
+puts buster.take_a_bow
